95f0c6ee7dd8509cae3cfb4bec685b828113a216
347Online | Katie Janzen <katiejanzen@347online.me>
2025-02-01 02:43:57 -0600

    Basic CLI with Clap

--- um-asm/src/main.rs
+++ um-asm/src/main.rs
@@ -1,9 +1,27 @@
 use um_asm::assemble;
 
-fn main() -> anyhow::Result<()> {
-    let instrs = assemble("foo.umasm")?;
+use clap::Parser;
 
-    dbg!(instrs);
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value_t = false)]
+    debug: bool,
+
+    #[arg(required = true)]
+    files: Vec<String>,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    for file in args.files {
+        let instrs = assemble(&file)?;
+
+        if args.debug {
+            dbg!(instrs);
+        }
+    }
 
     Ok(())
 }
