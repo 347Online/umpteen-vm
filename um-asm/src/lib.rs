cd5618db8edef519df7eb2db15e5291bb4e6c7b8
347Online | Katie Janzen <katiejanzen@347online.me>
2025-02-01 02:13:53 -0600

    Better assembling thanks to regex

---
+++ um-asm/src/lib.rs
@@ -0,0 +1,40 @@
+#![feature(if_let_guard)]
+
+use anyhow::anyhow;
+use regex::Regex;
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+use um_vm::{Instr, Value};
+
+pub fn assemble() -> anyhow::Result<Vec<Instr>> {
+    let mut instrs = vec![];
+
+    let push_re = Regex::new(r"PUSH\s+(\d+(?:\.(?:\d*)?)?)").unwrap();
+
+    let file = File::open("foo.umasm")?;
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        match line?.as_str() {
+            "" => (),
+            "POP" => instrs.push(Instr::Pop),
+            "ADD" => instrs.push(Instr::Add),
+            "PRINTLN" => instrs.push(Instr::PrintLn),
+
+            // PUSH
+            line if let Some(caps) = push_re.captures(line) => {
+                let num: f64 = caps
+                    .get(1)
+                    .ok_or(anyhow!("Missing argument for push"))?
+                    .as_str()
+                    .parse()
+                    .unwrap();
+                instrs.push(Instr::Push(Value::Number(num)));
+            }
+
+            line => Err(anyhow!("Unknown instruction `{line}`"))?,
+        }
+    }
+
+    Ok(instrs)
+}
