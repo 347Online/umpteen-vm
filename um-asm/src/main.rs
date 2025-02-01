859f623ba9d243e3a65ac41b74dec6f8f4d46086
347Online | Katie Janzen <katiejanzen@347online.me>
2025-02-01 02:51:05 -0600

    CLI tweaks

--- um-asm/src/main.rs
+++ um-asm/src/main.rs
@@ -5,9 +5,11 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
+    /// Print debug IR
     #[arg(short, long, default_value_t = false)]
     debug: bool,
 
+    /// File(s) to assemble
     #[arg(required = true)]
     files: Vec<String>,
 }
